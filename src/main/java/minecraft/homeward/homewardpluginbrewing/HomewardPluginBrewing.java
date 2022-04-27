5e85c7fb378f9d242738c55e3ecc4f5c76729111
Caishangqi <2470159730@qq.com>
2022-04-27 08:43:07 -0700

    Fist Caizii commit

--- src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
+++ src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
@@ -2,11 +2,13 @@ package minecraft.homeward.homewardpluginbrewing;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class HomewardPluginBrewing extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getLogger().log(Level.INFO,"嘻嘻");
     }
 
 }
