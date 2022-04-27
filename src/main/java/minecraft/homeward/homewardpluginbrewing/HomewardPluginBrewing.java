63e064e721e6baf84c5d77bddca02eac92d4f375
Caishangqi <2470159730@qq.com>
2022-04-27 09:16:45 -0700

    readme.md

--- src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
+++ src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
@@ -6,9 +6,20 @@ import java.util.logging.Level;
 
 public final class HomewardPluginBrewing extends JavaPlugin {
 
+    //常量部分
+    public final String PLUGIN_NAME = "酿酒";
+
+
     @Override
     public void onEnable() {
-        getLogger().log(Level.INFO,"嘻嘻");
+        getLogger().log(Level.INFO, "嘻嘻");
     }
 
+    /**
+     * 关闭逻辑
+     */
+    @Override
+    public void onDisable() {
+
+    }
 }
