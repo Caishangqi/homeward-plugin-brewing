d718773898c61ca3b716bba83ab8d4e50936e3cb
Ba1oretto <sunjiamu@outlook.com>
2022-04-28 00:34:11 +0900

    Initial commit

---
+++ src/main/java/minecraft/homeward/homewardpluginbrewing/HomewardPluginBrewing.java
@@ -0,0 +1,12 @@
+package minecraft.homeward.homewardpluginbrewing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomewardPluginBrewing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
